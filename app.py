1a51843b93a7d042ec744122ccaad5efd9203f49
Ekaterina <katyshik2000@yandex.com>
2022-11-08 20:39:49 +0300

    commit

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, url_for
-
-# DATABASE = '/flaskpj/flsite.db'
-DEBUG = True
-SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
-MAX_CONTENT_LENGTH = 1024 * 1024
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-# app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
-app.config['SECRET_KEY'] = '602854b15c5edb8cd51f27ac95b6df57c9ab37bf'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/wishlistsql'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'fdgfh78@#5?>gfhf89dx,v06'
+app_context = app.app_context()
+app_context.push()
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    psw = db.Column(db.String(200), nullable=False)
+    birthdate = db.Column(db.DateTime)
