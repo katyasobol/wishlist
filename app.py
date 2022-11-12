f64dbf026fef31b20a5cfa93e56c7ab03aca0615
Ekaterina <katyshik2000@yandex.com>
2022-11-12 23:27:04 +0300

    commit

--- app.py
+++ app.py
@@ -1,28 +1,69 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.security import check_password_hash, generate_password_hash
-from database import User, db
+from database import User, Profile
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import current_user, LoginManager, login_required
+import psycopg2
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:10122000kot@localhost/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'fdgfh78@#6?>gfhf80dx,v06'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
-@app.route('/register', methods=('POST', 'GET'))
+@login_manager.user_loader
+def load_user(user_id):
+    print("load_user")
+    return user_id
+
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == "POST":
         try:
             hash = generate_password_hash(request.form['psw'])
             u = User(user=request.form['username'], email=request.form['email'], psw=hash)
             db.session.add(u)
-            db.session.flush()
             db.session.commit()
         except:
             db.session.rollback()
             print("Ошибка добавления в БД")
+            return 'you ot registered'
 
-        return redirect(url_for('profile'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Регистрация')
 
 @app.route('/profile', methods=('POST', 'GET'))
 def profile():
-    return render_template('profile.html', title='Профайл')
+    if current_user.is_authenticated:
+        return render_template('profile.html', title='Профайл')
+    return redirect(url_for('login'))
+
+
+@app.route('/profile/update', methods=('POST', 'GET'))
+@login_required
+def update():
+    if request.method == "POST":
+        try:
+            p = Profile(firstname=request.form['firstname'], lastname=request.form['lastname'], birthdate=request.form['birthdate'])
+            db.session.add(p)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print("Ошибка добавления в БД")
+        return redirect(url_for('profile'))
+    return render_template('profileupd.html', title='Настройки аккаунта')
+
+
+@app.route('/login', methods=('POST', 'GET')) 
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        psw = request.form.get('psw')
+        if username == User.query.get(0) and check_password_hash(psw, User.psw.get(username.id)):
+            return redirect(url_for('profile'))
+        flash("Неверная пара логин/пароль", "error")
+    return render_template('login.html')      
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+import psycopg2
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:10122000kot@localhost/wishlistsql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:10122000kot@localhost/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'fdgfh78@#5?>gfhf89dx,v06'
+app.secret_key = 'fdgfh78@#5?>gfhf80dx,v06'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -20,12 +22,45 @@ class User(db.Model):
         self.psw = psw
     
     def __repr__(self):
-        return f'<users {self.id}'
+        return f'<user {self.id}>'
+
+
+class Profile(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80))
+    lastname = db.Column(db.String(80))
+    birthdate = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, firstname, lastname, birthdate, user_id):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.birthdate = birthdate
+        self.user_id = user_id
+
+
+    def __repr__(self):
+        return f'<profile {self.id}>'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == "POST":
+        try:
+            hash = generate_password_hash(request.form['psw'])
+            u = User(user=request.form['username'], email=request.form['email'], psw=hash)
+            db.session.add(u)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print("Ошибка добавления в БД")
+            return 'you ot registered'
+
+        return render_template('login.html')
+    return render_template('register.html', title='Регистрация')
 
 db.init_app(app)
 with app.app_context():
     db.create_all()
 
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
